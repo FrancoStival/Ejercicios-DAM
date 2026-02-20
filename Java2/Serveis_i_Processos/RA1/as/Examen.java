ab97786205d37b47887e6a0d6f8205892e32e7c9
Franco Stival <franco.stival@gmail.com>
2026-02-20 14:52:45 +0100

    Create Examen.java

---
+++ Java2/Serveis_i_Processos/RA1/as/Examen.java
@@ -0,0 +1,216 @@
+import java.util.*;
+
+class Ogre implements Runnable {
+    private int vida = 4000;
+    private List<Heroi> herois;
+    private final Random rnd = new Random();
+
+    public Ogre(List<Heroi> herois){
+        this.herois = herois;
+    }
+
+    public synchronized void rebreDany(int dany, String nom){
+        if(vida <= 0){
+            System.out.println("El ogro ha muerto. Los heroes han ganado");
+            return;
+        }
+        vida -= dany;
+        if (vida < 0) vida = 0;
+        System.out.println(nom + " fa " + dany + " de dany. Ogre: " + vida);
+    }
+
+    public synchronized boolean estaViu(){
+        return vida > 0;
+    }
+
+    public void run(){
+        while(estaViu()){
+            boolean algunViu = false;
+            for (Heroi h : herois){
+                if (h.estaViu()){
+                    algunViu = true;
+                    break;
+                }
+            }
+
+            if (!algunViu){
+                System.out.println("Todos muertos");
+            }
+
+            Heroi defensor = herois.get(rnd.nextInt(herois.size())); // Saca un numero random en base al tamaño del array de los heroes y ese Index lo usa para atacar
+            if(defensor.estaViu()){
+                defensor.rebreAtac(20 + rnd.nextInt(21));
+            }
+
+            try {
+                Thread.sleep(400 + rnd.nextInt(400));
+            } catch (Exception e) {}
+        }
+    }
+}
+
+abstract class Heroi implements Runnable{
+    protected  String nom;
+    protected  int vida = 100;
+    protected  Ogre ogre;
+    protected  Random rnd = new Random();
+
+    public Heroi(String nom, Ogre ogre){
+        this.nom = nom;
+        this.ogre = ogre;
+    }
+    public synchronized boolean estaViu(){ // Esto es syncronized pq tambien tiene que poder acceder la Sanadora
+        return vida > 0;
+    }
+
+    public synchronized void rebreAtac(int dany){
+        if (vida <= 0) return;
+        
+        if(rnd.nextInt(100) < 20){
+            System.out.println(nom + " esquiva l'atac");
+            return;
+        }
+
+        vida -= dany;
+        if (vida <= 0) { // Si el daño es mortal
+            vida = 0; // ( esto es por si hace overkill)
+            System.out.println(nom + " cae KO y espera a Eylin"); // Muere
+
+            try {
+                wait(); // Espera a ser revivido
+            } catch (InterruptedException e) {}
+
+            System.out.println(nom + " ha sido revivido y vuelve al combate"); // Revive
+        } else {
+            System.out.println(nom + " rep " + dany + " de dany. Vida: " + vida);
+        }
+    }
+}
+
+
+class Guerrer extends Heroi {
+    public Guerrer(String nom, Ogre ogre) {
+        super(nom, ogre);
+    }
+
+    public void run() {
+        while (ogre.estaViu()) {
+            synchronized (this) {
+                if (vida > 0) {
+                    ogre.rebreDany(70 + rnd.nextInt(30), nom);
+                }
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {}
+        }
+    }
+}
+
+class Mag extends Heroi {
+    public Mag(String nom, Ogre ogre) {
+        super(nom, ogre);
+    }
+
+    public void run() {
+        while (ogre.estaViu()) {
+            synchronized (this) {
+                if (vida > 0) {
+                    ogre.rebreDany(30 + rnd.nextInt(20), nom);
+                }
+            }
+            try {
+                Thread.sleep(600);
+            } catch (InterruptedException e) {}
+        }
+    }
+}
+
+class Picar extends Heroi {
+    public Picar(String nom, Ogre ogre) {
+        super(nom, ogre);
+    }
+
+    public void run() {
+        while (ogre.estaViu()) {
+            synchronized (this) {
+                if (vida > 0) {
+                    ogre.rebreDany(10 + rnd.nextInt(15), nom);
+                }
+            }
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {}
+        }
+    }
+}
+
+class Sanadora implements Runnable{
+    private final List<Heroi> herois;
+    private int mana = 80;
+
+    public Sanadora(List<Heroi> herois){
+        this.herois = herois;
+    }
+
+    public void run(){
+        while(true){
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+            }
+
+            if(mana < 80){
+                mana++;
+            }
+
+            for( Heroi h : herois){
+                synchronized (h) {
+                    if(h.vida <= 0 && mana >= 50){
+                        System.out.println("Eylin cura a " + h.nom);
+                        mana -= 50;
+                        h.vida = 50;
+                        h.notify();
+                    }
+                }
+            }
+        }
+    }
+}
+
+public class Examen{
+    public static void main(String[] args) {
+        List<Heroi> herois = new ArrayList<>();
+
+        Ogre ogre = new Ogre(herois);
+
+        Heroi h1 = new Guerrer("Dalinar", ogre);
+        Heroi h2 = new Mag("Kaladin", ogre);
+        Heroi h3 = new Picar("Vin", ogre);
+
+        herois.add(h1);
+        herois.add(h2);
+        herois.add(h3);
+
+        Sanadora lift = new Sanadora(herois);
+
+        Thread tOgre = new Thread(ogre);
+        Thread t1 = new Thread(h1);
+        Thread t2 = new Thread(h2);
+        Thread t3 = new Thread(h3);
+        Thread tLift = new Thread(lift);
+
+        tOgre.setPriority(Thread.MAX_PRIORITY);
+        t1.setPriority(Thread.NORM_PRIORITY);
+        t2.setPriority(Thread.NORM_PRIORITY);
+        t3.setPriority(Thread.NORM_PRIORITY);
+        tLift.setPriority(Thread.NORM_PRIORITY);
+
+        tOgre.start();
+        t1.start();
+        t2.start();
+        t3.start();
+        tLift.start();
+
+    }
+}
